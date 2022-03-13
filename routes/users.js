f2f314f863d0be5a5a40498d713f2ee1b49301c1
KEITH\Keith <s0701092@gmail.com>
2022-03-13 17:37:19 +0800

    Test API

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Joycow HK API on here doamin!');
 });
 
 module.exports = router;
